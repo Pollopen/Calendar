9b43bcfdca3d4e3ae6568dfab7886056baca9c38
kevin.hedsand <kevinhedsand@yahoo.se>
2016-11-25 10:14:49 +0100

    massa saker

--- src/object/Calendar.java
+++ src/object/Calendar.java
@@ -3,5 +3,31 @@ package object;
 import javax.swing.JPanel;
 
 public class Calendar extends JPanel {
+	int cal_id;
+	int creator_id;
+	String name;
+	String description;
+	String created;
+	String edited;
+	int Notification;
+	
+	public Calendar(int caid,int crid, String cname, String cdesc, String ccreated, String cedited, int not){
+		cal_id=caid;
+		creator_id=crid;
+		name=cname;
+		description=cdesc;
+		created=ccreated;
+		edited=cedited;
+		Notification=not;
+	}
+
+	public int getCal_id() {
+		return cal_id;
+	}
+
+	public void setCal_id(int cal_id) {
+		this.cal_id = cal_id;
+	}
+	
 
 }
--- src/object/User.java
+++ src/object/User.java
@@ -1,11 +1,17 @@
 package object;
 
+import database.JavaDB;
+
 public class User {
 	int id;
 	String email;
 	String fname;
 	String sname;
 	String created;
+	Calendar[] calArray;
+	Event[] eventArray;
+	
+	private JavaDB db = new JavaDB("localhost","root","","calendar");
 	
 	public User(int userid, String useremail, String userfname, String usersname, String usercreated){
 		id=userid;
@@ -14,7 +20,23 @@ public class User {
 		sname=usersname;
 		created=usercreated;
 	}
-	
+	public void reloadarrays(/*Array for which calendars to ignore*/){
+		Object[][]data = db.getData("select * from Calendar where creator_id = "+id);
+		int numberofresults=data.length;
+		int eventnum;
+		calArray = new Calendar[numberofresults];
+		Object[][]data2 = db.getData("SELECT COUNT(event.event_id) FROM calendar INNER JOIN event ON calendar.cal_id = event.cal_id WHERE calendar.creator_id = 1");
+		eventArray = new Event[Integer.parseInt((String) data2[0][0])];
+		for(int i=0;i<=numberofresults;i++){
+			calArray[i]= new Calendar(Integer.parseInt((String) data[i][0]), Integer.parseInt((String) data[i][1]), (String) data[i][2], (String) data[i][3], (String) data[i][4], (String) data[i][5], Integer.parseInt((String) data[i][6]));
+			Object[][]data3 = db.getData("SELECT * FROM event where cal_id = "+calArray[i].getCal_id());
+			eventnum=data3.length;
+			for(int j=0;j<=eventnum;j++){
+				
+			}
+			
+		}
+	}
 	public void getAll() {
 		System.out.println(id);
 		System.out.println(email);
