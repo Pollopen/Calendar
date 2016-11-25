f070877e961699b3db7408ab190634d100bf108e
Nallieh <nallieh1001@gmail.com>
2016-11-25 13:01:23 +0100

    gridbaglayout implemented

--- src/views/Index.java
+++ src/views/Index.java
@@ -2,6 +2,8 @@ package views;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 
 import javax.swing.JPanel;
@@ -9,6 +11,8 @@ import javax.swing.JPanel;
 public class Index extends JPanel {
 
 	private JPanel mainPanel, leftPanel, rightPanel, upperLeftPanel, upperRightPanel;
+	private GridBagConstraints gbc;
+
 	private Window window;
 
 	public Index(Window window) {
@@ -17,9 +21,12 @@ public class Index extends JPanel {
 		// Main panel
 		mainPanel = new JPanel();
 		mainPanel.setPreferredSize(new Dimension(1400, 800));
-		mainPanel.setLayout(new GridLayout(2, 2));
+		mainPanel.setLayout(new GridBagLayout());
 		mainPanel.setVisible(true);
 		mainPanel.setBackground(new Color(255, 0, 255));
+		this.add(mainPanel);
+
+		gbc = new GridBagConstraints();
 
 		// Upper left panel
 		upperLeftPanel = new JPanel();
@@ -28,12 +35,22 @@ public class Index extends JPanel {
 		upperLeftPanel.setVisible(true);
 		upperLeftPanel.setBackground(new Color(255, 0, 0));
 
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+
+		mainPanel.add(upperLeftPanel, gbc);
+
 		// Upper right panel
 		upperRightPanel = new JPanel();
 		upperRightPanel.setPreferredSize(new Dimension(1200, 100));
 		upperRightPanel.setLayout(new GridLayout(2, 2));
 		upperRightPanel.setVisible(true);
 		upperRightPanel.setBackground(new Color(0, 255, 0));
+
+		gbc.gridx = 1;
+		gbc.gridy = 0;
+
+		mainPanel.add(upperRightPanel, gbc);
 
 		// Left panel
 		leftPanel = new JPanel();
@@ -42,19 +59,22 @@ public class Index extends JPanel {
 		leftPanel.setVisible(true);
 		leftPanel.setBackground(new Color(0, 0, 255));
 
-		// Left panel
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+
+		mainPanel.add(leftPanel, gbc);
+
+		// Right panel
 		rightPanel = new JPanel();
 		rightPanel.setPreferredSize(new Dimension(1200, 700));
 		rightPanel.setLayout(new GridLayout(2, 2));
 		rightPanel.setVisible(true);
 		rightPanel.setBackground(new Color(100, 100, 100));
 
-		this.add(mainPanel);
+		gbc.gridx = 1;
+		gbc.gridy = 1;
 
-		mainPanel.add(upperLeftPanel);
-		mainPanel.add(upperRightPanel);
-		mainPanel.add(leftPanel);
-		mainPanel.add(rightPanel);
+		mainPanel.add(rightPanel, gbc);
 
 	}
 
