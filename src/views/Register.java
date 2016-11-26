a3c883829983621a175b0aedcd81465b32fb9b33
Nallieh <nallieh1001@gmail.com>
2016-11-26 12:35:52 +0100

    register grafiskt

--- src/views/Register.java
+++ src/views/Register.java
@@ -1,9 +1,16 @@
 package views;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -17,36 +24,146 @@ import database.JavaDB;
 
 public class Register extends JPanel {
 
+	private JPanel main, form;
 	private JLabel regHere, emailLabel, passLabel, passConfLabel, fnameLabel, snameLabel;
 	private JTextField emailField, fnameField, snameField;
 	private JPasswordField passField, passConfField;
 	private JButton regButton;
 	private JavaDB db = new JavaDB("localhost", "root", "", "calendar");
+	private GridBagConstraints gbc;
 	private int user_id;
 	private Window window;
 
 	public Register(Window window) {
 		this.window = window;
+		
+		gbc = new GridBagConstraints();
+		
+		main = new JPanel();
+		main.setPreferredSize(new Dimension(1400, 800));
+		main.setLayout(new GridBagLayout());
+		
+		this.add(main);
+		
+		form = new JPanel();
+		form.setLayout(new GridBagLayout());
+		form.setPreferredSize(new Dimension(480,600));
+		form.setBorder(BorderFactory.createLineBorder(Color.black));
+		form.setVisible(true);
+		
+		main.add(form);
 
-		setLayout(new GridLayout(20, 1));
+		
+		
+		
 		// Stor text at top
 		regHere = new JLabel("Registrera dig här!", JLabel.CENTER);
+		regHere.setFont(new Font("Serif", Font.PLAIN, 25));
+		
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.insets = new Insets(0, 0, 75, 0);
+		
+		form.add(regHere, gbc);
+		
 		// Email
 		emailLabel = new JLabel("Email: ");
-		emailField = new JTextField(40);
+		emailLabel.setFont(new Font("Serif", Font.PLAIN, 20));
+		emailField = new JTextField();
+		emailField.setPreferredSize(new Dimension(300, 30));
+		
+		
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		gbc.insets = new Insets(0, 0, 10, 0);
+
+		form.add(emailLabel, gbc);
+		
+		gbc.gridx = 0;
+		gbc.gridy = 2;
+		gbc.insets = new Insets(0, 0, 10, 0);
+		
+		form.add(emailField, gbc);
+		
 		// Password + password confirmation
 		passLabel = new JLabel("Lösenord: ");
-		passField = new JPasswordField(40);
+		passLabel.setFont(new Font("Serif", Font.PLAIN, 20));
+		passField = new JPasswordField();
+		passField.setPreferredSize(new Dimension(300, 30));
 		passConfLabel = new JLabel("Lösenord igen: ");
-		passConfField = new JPasswordField(40);
+		passConfLabel.setFont(new Font("Serif", Font.PLAIN, 20));
+		passConfField = new JPasswordField();
+		passConfField.setPreferredSize(new Dimension(300, 30));
+		
+		gbc.gridx = 0;
+		gbc.gridy = 3;
+		gbc.insets = new Insets(0, 0, 10, 0);
+
+		form.add(passLabel, gbc);
+		
+		gbc.gridx = 0;
+		gbc.gridy = 4;
+		gbc.insets = new Insets(0, 0, 10, 0);
+		
+		form.add(passField, gbc);
+		
+		gbc.gridx = 0;
+		gbc.gridy = 5;
+		gbc.insets = new Insets(0, 0, 10, 0);
+		
+		form.add(passConfLabel, gbc);
+		
+		gbc.gridx = 0;
+		gbc.gridy = 6;
+		gbc.insets = new Insets(0, 0, 10, 0);
+		
+		form.add(passConfField, gbc);
+		
 		// First name + last name
 		fnameLabel = new JLabel("Förnamn: ");
-		fnameField = new JTextField(40);
+		fnameLabel.setFont(new Font("Serif", Font.PLAIN, 20));
+		fnameField = new JTextField();
+		fnameField.setPreferredSize(new Dimension(300, 30));
 		snameLabel = new JLabel("Efternamn: ");
-		snameField = new JTextField(40);
+		snameLabel.setFont(new Font("Serif", Font.PLAIN, 20));
+		snameField = new JTextField();
+		snameField.setPreferredSize(new Dimension(300, 30));
+		
+		gbc.gridx = 0;
+		gbc.gridy = 7;
+		gbc.insets = new Insets(0, 0, 10, 0);
+
+		form.add(fnameLabel, gbc);
+		
+		gbc.gridx = 0;
+		gbc.gridy = 8;
+		gbc.insets = new Insets(0, 0, 10, 0);
+		
+		form.add(fnameField, gbc);
+		
+		gbc.gridx = 0;
+		gbc.gridy = 9;
+		gbc.insets = new Insets(0, 0, 10, 0);
+		
+		form.add(snameLabel, gbc);
+		
+		gbc.gridx = 0;
+		gbc.gridy = 10;
+		gbc.insets = new Insets(0, 0, 10, 0);
+		
+		form.add(snameField, gbc);
+		
 		// Reg button
 		regButton = new JButton("Registrera dig!");
 
+		gbc.gridx = 0;
+		gbc.gridy = 11;
+		gbc.insets = new Insets(0, 0, 10, 0);
+		
+		form.add(regButton, gbc);
+		
+		
+		
 		ListenForButton lForButton = new ListenForButton();
 
 		// Tell Java that you want to be alerted when an event
@@ -54,18 +171,6 @@ public class Register extends JPanel {
 
 		regButton.addActionListener(lForButton);
 
-		add(regHere);
-		add(emailLabel);
-		add(emailField);
-		add(passLabel);
-		add(passField);
-		add(passConfLabel);
-		add(passConfField);
-		add(fnameLabel);
-		add(fnameField);
-		add(snameLabel);
-		add(snameField);
-		add(regButton);
 	}
 
 	private class ListenForButton implements ActionListener {
