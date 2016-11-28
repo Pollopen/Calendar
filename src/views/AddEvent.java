b6b08c91cb36d280564620a9930e424f04038f6b
Nallieh <nallieh1001@gmail.com>
2016-11-28 15:10:45 +0100

    började lägga på labels, textfields, osv i addevent

--- src/views/AddEvent.java
+++ src/views/AddEvent.java
@@ -1,19 +1,23 @@
 package views;
 
-import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSpinner;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.SpinnerDateModel;
 
 public class AddEvent extends JPanel {
 	private JPanel main, top, center, centerLeft, centerRight;
 	private JLabel nameLabel, locationLabel, startTimeLabel, endTimeLabel, descriptionLabel;
 	private JTextField nameField, locationField;
+	private JSpinner.DateEditor startDateSpinner, endDateSpinner;
+	private JSpinner startTimeSpinner, endTimeSpinner;
 	private JTextArea descriptionArea;
 	private GridBagConstraints gbc;
 	private Index index;
@@ -21,6 +25,8 @@ public class AddEvent extends JPanel {
 	public AddEvent(Index index) {
 		this.index = index;
 		gbc = new GridBagConstraints();
+
+		// Panel management
 
 		main = new JPanel();
 		main.setPreferredSize(new Dimension(1175, 725));
@@ -33,7 +39,7 @@ public class AddEvent extends JPanel {
 		top = new JPanel();
 		top.setPreferredSize(new Dimension(1175, 20));
 		top.setLayout(new GridBagLayout());
-		top.setBackground(new Color(255, 0, 0));
+		// top.setBackground(new Color(255, 0, 0));
 		top.setVisible(true);
 
 		gbc.gridx = 0;
@@ -44,7 +50,7 @@ public class AddEvent extends JPanel {
 		center = new JPanel();
 		center.setPreferredSize(new Dimension(1175, 705));
 		center.setLayout(new GridBagLayout());
-		center.setBackground(new Color(0, 255, 0));
+		// center.setBackground(new Color(0, 255, 0));
 		center.setVisible(true);
 
 		gbc.gridx = 0;
@@ -55,24 +61,88 @@ public class AddEvent extends JPanel {
 		centerLeft = new JPanel();
 		centerLeft.setPreferredSize(new Dimension(700, 705));
 		centerLeft.setLayout(new GridBagLayout());
-		centerLeft.setBackground(new Color(0, 255, 0));
+		// centerLeft.setBackground(new Color(0, 255, 0));
 		centerLeft.setVisible(true);
-		
+
 		gbc.gridx = 0;
 		gbc.gridy = 0;
-		
+
 		center.add(centerLeft, gbc);
 
 		centerRight = new JPanel();
 		centerRight.setPreferredSize(new Dimension(475, 705));
 		centerRight.setLayout(new GridBagLayout());
-		centerRight.setBackground(new Color(0, 0, 255));
+		// centerRight.setBackground(new Color(0, 0, 255));
 		centerRight.setVisible(true);
 
 		gbc.gridx = 1;
 		gbc.gridy = 0;
 
 		center.add(centerRight, gbc);
+
+		// Everything on panels
+
+		// Event label + event field
+		nameLabel = new JLabel("Event namn");
+		nameLabel.setFont(new Font("Serif", Font.PLAIN, 20));
+
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+
+		centerLeft.add(nameLabel, gbc);
+
+		nameField = new JTextField();
+		nameField.setPreferredSize(new Dimension(300, 30));
+
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+
+		centerLeft.add(nameField, gbc);
+
+		// Event location label + location field
+
+		locationLabel = new JLabel("Plats");
+		locationLabel.setFont(new Font("Serif", Font.PLAIN, 20));
+
+		gbc.gridx = 0;
+		gbc.gridy = 2;
+
+		centerLeft.add(locationLabel, gbc);
+
+		locationField = new JTextField();
+		locationField.setPreferredSize(new Dimension(300, 30));
+
+		gbc.gridx = 0;
+		gbc.gridy = 3;
+
+		centerLeft.add(locationField, gbc);
+
+		// Event start time + end time
+
+		startTimeLabel = new JLabel("Start på event");
+		startTimeLabel.setFont(new Font("Serif", Font.PLAIN, 20));
+
+		gbc.gridx = 0;
+		gbc.gridy = 4;
+
+		centerLeft.add(startTimeLabel, gbc);
+
+		endTimeLabel = new JLabel("Slut på event");
+		endTimeLabel.setFont(new Font("Serif", Font.PLAIN, 20));
+
+		gbc.gridx = 0;
+		gbc.gridy = 5;
+
+		centerLeft.add(endTimeLabel, gbc);
+			
+		startTimeSpinner = new JSpinner(new SpinnerDateModel());
+		startDateSpinner = new JSpinner.DateEditor(startTimeSpinner, "HH:mm:ss");
+		startDateSpinner.setPreferredSize(new Dimension(300,30));
+		
+		gbc.gridx = 0;
+		gbc.gridy = 6;
+		
+		centerLeft.add(startDateSpinner, gbc);
 
 	}
 
