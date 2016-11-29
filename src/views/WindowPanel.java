1ec41c0ea9ce6d189c782163f4f776fc207b780b
kevin.hedsand <kevinhedsand@yahoo.se>
2016-11-29 08:32:22 +0100

    moved index
    *TODO*
    Fix register

--- src/views/WindowPanel.java
+++ src/views/WindowPanel.java
@@ -24,7 +24,8 @@ import object.User;
 
 public class WindowPanel extends JPanel {
 	
-	private JPanel form, main, center;
+	private JPanel form, main, center, mainPanel, leftPanel, addEventButtonPanel, overviewPanel, CalendarChoicePanel, placeholderPanel,
+	rightPanel, calendarPanel, upperLeftPanel, upperRightPanel;
 	private JLabel loginLabel, emailLabel, passLabel, regLabel, regHere, passConfLabel, fnameLabel, snameLabel;
 	private JTextField emailField, fnameField, snameField;
 	private JPasswordField passField, passConfField;
@@ -51,6 +52,117 @@ public class WindowPanel extends JPanel {
 		//center.removeAll();
 		//center.add(new Index(this));
 		//center.updateUI();
+		center.removeAll();
+		MenuList menu = new MenuList();
+		window.setJMenuBar(menu.createMenuBar(window));
+
+		// Main panel
+		mainPanel = new JPanel();
+		mainPanel.setPreferredSize(new Dimension(1400, 800));
+		mainPanel.setLayout(new GridBagLayout());
+		mainPanel.setVisible(true);
+		mainPanel.setBackground(new Color(0, 0, 255));
+		center.add(mainPanel);
+
+		gbc = new GridBagConstraints();
+
+		// Upper left panel
+		upperLeftPanel = new JPanel();
+		upperLeftPanel.setPreferredSize(new Dimension(200, 50));
+		upperLeftPanel.setLayout(new GridLayout(2, 2));
+		upperLeftPanel.setVisible(true);
+		upperLeftPanel.setBackground(new Color(255, 0, 0));
+
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+
+		mainPanel.add(upperLeftPanel, gbc);
+
+		// Upper right panel
+		upperRightPanel = new JPanel();
+		upperRightPanel.setPreferredSize(new Dimension(1200, 50));
+		upperRightPanel.setLayout(new GridLayout(2, 2));
+		upperRightPanel.setVisible(true);
+		upperRightPanel.setBackground(new Color(0, 255, 0));
+
+		gbc.gridx = 1;
+		gbc.gridy = 0;
+
+		mainPanel.add(upperRightPanel, gbc);
+
+		// Left panel
+		leftPanel = new JPanel();
+		leftPanel.setPreferredSize(new Dimension(200, 750));
+		leftPanel.setLayout(new GridBagLayout());
+		leftPanel.setVisible(true);
+		leftPanel.setBackground(new Color(0, 0, 255));
+
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+
+		mainPanel.add(leftPanel, gbc);
+
+		addEventButtonPanel = new JPanel();
+		addEventButtonPanel.setPreferredSize(new Dimension(200, 100));
+		addEventButtonPanel.setVisible(true);
+		addEventButtonPanel.setBackground(new Color(0, 0, 255));
+
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+
+		leftPanel.add(addEventButtonPanel, gbc);
+
+		overviewPanel = new JPanel();
+		overviewPanel.setPreferredSize(new Dimension(200, 200));
+		overviewPanel.setVisible(true);
+		overviewPanel.setBackground(new Color(100, 100, 100));
+
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+
+		leftPanel.add(overviewPanel, gbc);
+
+		CalendarChoicePanel = new JPanel();
+		CalendarChoicePanel.setPreferredSize(new Dimension(200, 200));
+		CalendarChoicePanel.setVisible(true);
+		CalendarChoicePanel.setBackground(new Color(255, 0, 255));
+
+		gbc.gridx = 0;
+		gbc.gridy = 2;
+
+		leftPanel.add(CalendarChoicePanel, gbc);
+
+		overviewPanel = new JPanel();
+		overviewPanel.setPreferredSize(new Dimension(200, 200));
+		overviewPanel.setVisible(true);
+		overviewPanel.setBackground(new Color(255, 255, 0));
+
+		gbc.gridx = 0;
+		gbc.gridy = 3;
+
+		leftPanel.add(overviewPanel, gbc);
+
+		// Right panel
+		rightPanel = new JPanel();
+		rightPanel.setPreferredSize(new Dimension(1200, 750));
+		rightPanel.setLayout(new GridBagLayout());
+		rightPanel.setVisible(true);
+		rightPanel.setBackground(new Color(150, 150, 150));
+
+		gbc.gridx = 1;
+		gbc.gridy = 1;
+
+		mainPanel.add(rightPanel, gbc);
+		center.updateUI();
+		/*
+		 * calendarPanel = new JPanel(); calendarPanel.setPreferredSize(new
+		 * Dimension(1175, 725)); calendarPanel.setVisible(true);
+		 * calendarPanel.setBackground(new Color(255, 0, 0));
+		 * 
+		 * gbc.gridx = 0; gbc.gridy = 0;
+		 * 
+		 * rightPanel.add(calendarPanel, gbc);
+		 */
 	}
 
 	public void getRegisterPage() {
@@ -310,6 +422,18 @@ public class WindowPanel extends JPanel {
 		loginButton.addActionListener(lForButton);
 		registerPageButton.addActionListener(lForButton);
 		center.updateUI();
+	}
+	public void getAddEventPage() {
+		rightPanel.removeAll();
+		
+		
+		
+		
+		
+		
+		
+		
+		rightPanel.updateUI();
 	}
 	private class ListenForButton implements ActionListener {
 
