46a793782ccd770bafb3f2364bb65e2256623dcb
kevin.hedsand <kevinhedsand@yahoo.se>
2016-11-29 09:13:18 +0100

    register function

--- src/views/Register.java
+++ src/views/Register.java
@@ -23,23 +23,10 @@ import org.mindrot.jbcrypt.BCrypt;
 import database.JavaDB;
 
 public class Register extends JPanel {
+	
+	private static JavaDB db = new JavaDB();
 
-	private JPanel main, form;
-	private JLabel regHere, emailLabel, passLabel, passConfLabel, fnameLabel, snameLabel, loginLabel;
-	private JTextField emailField, fnameField, snameField;
-	private JPasswordField passField, passConfField;
-	private JavaDB db = new JavaDB();
-	private JButton regButton, loginButton;
-	private GridBagConstraints gbc;
-	private int user_id;
-	private Window window;
-
-	public static boolean register(){
-		String email = emailField.getText();
-		char[] pass1 = passField.getPassword();
-		char[] pass2 = passConfField.getPassword();
-		String fname = fnameField.getText();
-		String sname = snameField.getText();
+	public static boolean register(Window window, String fname, String sname, String email, char[] pass1, char[] pass2){
 		int pass1Length = pass1.length;
 		int pass2Length = pass2.length;
 		boolean passwordMatch = true;
@@ -51,34 +38,33 @@ public class Register extends JPanel {
 				if (pass1[i - 1] == pass2[i - 1]) {
 				} else {
 					passwordMatch = false;
+					return false;
 				}
 			}
 
 		} else {
 			passwordMatch = false;
+			return false;
 		}
 
 		if (passwordMatch) {
 			hashed = BCrypt.hashpw(String.valueOf(pass1), BCrypt.gensalt());
 			String SQL = "INSERT INTO user(email,password,fname,sname) VALUES('" + email + "','" + hashed
 					+ "','" + fname + "','" + sname + "');";
-
 			db.execute(SQL);
 
 			SQL = "SELECT * FROM user WHERE email = '" + email + "'";
 			Object[][] data = db.getData(SQL);
-
-			user_id = Integer.parseInt((String) data[0][0]);
+			int user_id = Integer.parseInt((String) data[0][0]);
 
 			SQL = "INSERT INTO calendar(creator_id,name,description,notification) VALUES('" + user_id
 					+ "','Min kalender','Det här är din standard kalender',1);";
-
 			db.execute(SQL);
 
 			JOptionPane.showMessageDialog(window, "Tack för att du registrerade dig, " + fname + " " + sname,
 					"Registrering lyckades!", JOptionPane.INFORMATION_MESSAGE);
-
-
+			return true;
 		}
+		return false;
 	}
 }
