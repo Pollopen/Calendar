86251af6826c548136fa1496bbd892c9313dc7d1
kevin.hedsand <kevinhedsand@yahoo.se>
2016-12-05 15:19:00 +0100

    Spinner to choose day/month view

--- src/views/MonthView.java
+++ src/views/MonthView.java
@@ -9,6 +9,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import controller.StateMachine;
@@ -19,23 +20,60 @@ public class MonthView extends JPanel{
 	private String date;
 	private Date focusedDate;
 	private DateFormat getFocusDate = new SimpleDateFormat("yyyy/MM/dd");
+	private DateFormat getWeekDay = new SimpleDateFormat("u");
+	private DateFormat getMonth = new SimpleDateFormat("M");
+	private int weekDay, j;
 	public MonthView(StateMachine SM){
 		this.SM=SM;
 		setPreferredSize(new Dimension(1175, 725));
 		setLayout(new GridBagLayout());
 		setBackground(new Color(0, 255, 255));
 		setVisible(true);
-		
+
 		date=SM.getFocusedDate();
+		
 		try {
 			focusedDate=getFocusDate.parse(date);
 		} catch (ParseException e) {
 			System.out.println("Date Conversion failed!");
 		}
-		
+		weekDay = Integer.parseInt(getWeekDay.format(focusedDate));
+		System.out.println(weekDay);
 		
 		month = new JPanel();
 		month.setLayout(new GridLayout(7,7));
+		month.setVisible(true);
+		month.setPreferredSize(new Dimension(1000, 600));
+		month.setBackground(new Color(255, 0, 0));
+		add(month);
+		month.add(new JLabel("Måndag"));
+		month.add(new JLabel("Tisdag"));
+		month.add(new JLabel("Onsdag"));
+		month.add(new JLabel("Torsdag"));
+		month.add(new JLabel("Fredag"));
+		month.add(new JLabel("Lördag"));
+		month.add(new JLabel("Söndag"));
+		if(weekDay!=1){
+			for (int i=0;i<(weekDay-1);i++){
+				JLabel empty = new JLabel("");
+				month.add(empty);
+			} 
+		}
+		
+		j=1;
+		while (true) {
+			JLabel dayNumber = new JLabel(Integer.toString(j));
+			j++;
+			month.add(dayNumber);
+			if(j>31){
+				break;
+			}
+		}
+		for (int i = 0; i < (11-weekDay); i++) {
+			JLabel empty = new JLabel("");
+			month.add(empty);
+		}
+		month.updateUI();
 		
 	}
 }
--- src/views/ViewChoice.java
+++ src/views/ViewChoice.java
@@ -4,9 +4,18 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerDateModel;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import controller.StateMachine;
 
@@ -16,6 +25,9 @@ public class ViewChoice extends JPanel {
 	private ListenForButton lForButton;
 	private StateMachine SM;
 	private WindowPanel wp;
+	private JSpinner dateChoice;
+	private DateFormat getFocusDate = new SimpleDateFormat("yyyy/MM/dd");
+	private Date focusedDate;
 	
 	public ViewChoice(StateMachine SM, WindowPanel wp){
 		this.SM=SM;
@@ -62,6 +74,26 @@ public class ViewChoice extends JPanel {
         gbc.gridx = 3;
 		gbc.gridy = 0;
         add(yearButton,gbc);
+        
+        try {
+			focusedDate=getFocusDate.parse(SM.getFocusedDate());
+		} catch (ParseException e) {
+			System.out.println("Date Conversion failed!");
+		}
+        
+        dateChoice = new JSpinner(new SpinnerDateModel(focusedDate, null, null,Calendar.DAY_OF_MONTH));
+        JSpinner.DateEditor dateEditor = new JSpinner.DateEditor(dateChoice, "dd/MM/yy");
+        dateChoice.setEditor(dateEditor);
+        
+		gbc.gridx = 4;
+		gbc.gridy = 0;
+		
+		add(dateChoice);
+		
+		ListenForSpinner lForSpinner = new ListenForSpinner();
+		
+
+		dateChoice.addChangeListener(lForSpinner);
 		
 	}
 	
@@ -93,6 +125,22 @@ public class ViewChoice extends JPanel {
 				wp.getViewViewer();
 				wp.getViewChoice();
 			}
+			
+		}
+	}
+	private class ListenForSpinner implements ChangeListener {
+
+		// This method is called when an event occurs
+
+		public void stateChanged(ChangeEvent e) {
+			if (e.getSource() == dateChoice) {
+				String temp = getFocusDate.format(dateChoice.getValue());
+				SM.setFocusedDate(temp);
+				System.out.println(temp);
+				wp.getViewViewer();
+				wp.getViewChoice();
+			}
+			// Check if the source of the event was the button
 			
 		}
 	}
