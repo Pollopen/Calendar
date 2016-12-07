99d2b93c82f0c960bb9d6b7b2dfd402e28841208
Nallieh <nallieh1001@gmail.com>
2016-12-07 15:09:33 +0100

    weekView funkar typ

--- src/views/ViewChoice.java
+++ src/views/ViewChoice.java
@@ -25,12 +25,16 @@ public class ViewChoice extends JPanel {
 	private GridBagConstraints gbc;
 	private GregorianCalendar gc;
 	private ListenForButton lForButton;
+	private SimpleDateFormat sdf;
 	private StateMachine SM;
 	private WindowPanel wp;
+	private Calendar cal;
 	private JSpinner dateChoice;
 	private DateFormat getFocusDate = new SimpleDateFormat("yyyy/MM/dd");
 	private Date focusedDate;
 	private int week;
+	private static int nextWeeksDate=7;
+
 
 	public ViewChoice(StateMachine SM, WindowPanel wp) {
 		this.SM = SM;
@@ -112,14 +116,157 @@ public class ViewChoice extends JPanel {
 
 	}
 	
-	
-	
-	private void addOneWeek()
+	public String addZero(int value)
 	{
-		gc = new GregorianCalendar();
-		week = gc.get(Calendar.WEEK_OF_YEAR);
-		week++;
-		gc.set(Calendar.WEEK_OF_YEAR, week);
+		if(value <= 9)
+			return "0"+value;
+		else
+			return ""+value;
+	}
+
+	public void getNextWeek() {
+
+		cal = Calendar.getInstance();
+
+		String todaysDate = SM.getFocusedDate();
+		String focusedDay = todaysDate.substring(8, 10);
+		String focusedMonth = todaysDate.substring(5, 7);
+		String focusedYear = todaysDate.substring(0, 4);
+
+		String addZeroToMonth = "0";
+		String addZeroToDay = "0";
+		String formatDate = "";
+		String forMonthJan = "01";
+//		int temp = Integer.parseInt(focusedMonth);
+//
+//		if (temp == 12) {
+//			temp -= 1;
+//		}
+//
+//		cal.set(Calendar.YEAR, Integer.parseInt(focusedYear));
+//		cal.set(Calendar.MONTH, temp);
+
+		System.out.println(focusedYear);
+		System.out.println(focusedMonth);
+
+		int year = cal.get(Calendar.YEAR);
+		int month = cal.get(Calendar.MONTH);
+		int lastDayOfMonth = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
+		int lastWeekOfYear = cal.getActualMaximum(Calendar.WEEK_OF_YEAR);
+		int weekOfYear = cal.get(Calendar.WEEK_OF_YEAR);
+
+		nextWeeksDate += 7;
+		
+		cal.set(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), nextWeeksDate);
+		
+		
+
+		formatDate = cal.get(Calendar.YEAR)+"/"+addZero(cal.get(Calendar.MONTH) + 1)+"/"+addZero(cal.get(Calendar.DAY_OF_MONTH));
+		
+//		if (lastDayOfMonth < nextWeeksDate) {
+//			nextWeeksDate = nextWeeksDate - lastDayOfMonth;
+//			month++;
+//
+//			if (month > 11) {
+//				month = 0;
+//				year++;
+//
+//				if (nextWeeksDate < 10) {
+//					addZeroToDay = addZeroToDay + nextWeeksDate;
+//
+//					formatDate = year + "/01/" + addZeroToDay;
+//				} else {
+//					formatDate = year + "/01/" + nextWeeksDate;
+//				}
+//
+//			} else if (nextWeeksDate < 10) {
+//				addZeroToDay = addZeroToDay + nextWeeksDate;
+//
+//				formatDate = year + "/0" + month + "/" + addZeroToDay;
+//			} else {
+//				formatDate = year + "/" + month + "/" + nextWeeksDate;
+//			}
+//		} else if (month < 10) {
+//			
+//			formatDate = year + "/0" + month + "/" + nextWeeksDate;
+//			
+//			if (nextWeeksDate < 10) {
+//				addZeroToDay = addZeroToDay + nextWeeksDate;
+//
+//				formatDate = year + "/0"+month+"/" + addZeroToDay;
+//			} else {
+//				formatDate = year + "/0"+month+"/" + nextWeeksDate;
+//			}
+//			
+//		} else {
+//			formatDate = year + "/" + month + "/" + nextWeeksDate;
+//		}
+
+		// if (lastDayOfMonth < nextWeeksDate)
+		// {
+		//
+		// if (month == 11)
+		// {
+		// month = 0;
+		// year++;
+		// }
+		// else
+		// {
+		// month++;
+		// }
+		//
+		// weekOfYear++;
+		//
+		// if (weekOfYear > lastWeekOfYear)
+		// {
+		// weekOfYear = 0;
+		// }
+		//
+		// nextWeeksDate = nextWeeksDate - lastDayOfMonth;
+		// addZeroToDay = addZeroToDay+nextWeeksDate;
+		// formatDate = year + "/" + month + "/0"+nextWeeksDate;
+		//
+		// }
+		// else
+		// {
+		// formatDate = year + "/" + month + "/" + addZeroToDay;
+		// }
+		//
+		//
+		// if(month < 10 && month > 0)
+		// {
+		// addZeroToMonth = addZeroToMonth+month;
+		//
+		// formatDate = year + "/" + addZeroToMonth + "/" + addZeroToDay;
+		//
+		// } else {
+		// formatDate = year + "/" + month + "/" + addZeroToDay;
+		// }
+		//
+		//
+		// if(month == 0)
+		// {
+		// formatDate = year + "/" + forMonthJan + "/" + addZeroToDay;
+		// } else {
+		//
+		// formatDate = year + "/" + month + "/" + nextWeeksDate;
+		//
+		// }
+
+		SM.setFocusedDate(formatDate);
+
+		System.out.println("------------------------------------------------------");
+		System.out.println("Sju dagar framåt: " + nextWeeksDate);
+		System.out.println(SM.getFocusedDate());
+//		System.out.println("Månad: " + month);
+//		System.out.println("År: " + year);
+//		System.out.println("Sista veckan på året: " + lastWeekOfYear);
+//		System.out.println("Sista dagen i månaden: " + lastDayOfMonth);
+		System.out.println("Formaterat datum " + formatDate);
+//		System.out.println("addZeroToDay: " + addZeroToDay);
+//		System.out.println("addZeroToMonth: " + addZeroToMonth);
+		System.out.println("------------------------------------------------------");
+
 	}
 
 	private class ListenForButton implements ActionListener {
@@ -156,6 +303,7 @@ public class ViewChoice extends JPanel {
 				wp.getViewChoice();
 			}
 			if (e.getSource() == nextWeekButton) {
+				getNextWeek();
 				SM.setActiveview(2);
 				wp.getViewViewer();
 				wp.getViewChoice();
@@ -175,7 +323,7 @@ public class ViewChoice extends JPanel {
 				SM.setFocusedDate(temp);
 				System.out.println(temp);
 				wp.getViewViewer();
-				//wp.getViewChoice();
+				// wp.getViewChoice();
 			}
 			// Check if the source of the event was the button
 
--- src/views/WeekView.java
+++ src/views/WeekView.java
@@ -22,12 +22,12 @@ public class WeekView extends JPanel {
 	private JPanel weekDaysPanel, weekDatePanel;
 	private JLabel weekDaysLabel, dayOfMonthLabel;
 	private Calendar cal;
-	private String date;
+	private String date, tempFocusedDay, tempFocusedMonth, tempFocusedYear;
 	private Date focusedDate;
 	private DateFormat getFocusDate = new SimpleDateFormat("yyyy-MM-dd");
 	private DateFormat getWeekDay = new SimpleDateFormat("u");
 	private DateFormat getMonth = new SimpleDateFormat("M");
-	private int weekDay;
+	private int weekDay, newDayDate, newMonthDate, newYearDate;
 
 	public WeekView(StateMachine SM) {
 		this.SM = SM;
@@ -36,16 +36,26 @@ public class WeekView extends JPanel {
 		this.setPreferredSize(new Dimension(1175, 725));
 		this.setVisible(true);
 
-		cal = new GregorianCalendar();
+		cal = Calendar.getInstance();
 		date = SM.getFocusedDate();
 
-		String tempFocusedDate = date.substring(8, 10);
-		System.out.println(tempFocusedDate);
+		tempFocusedDay = date.substring(8, 10);
+		newDayDate = Integer.parseInt(tempFocusedDay);
+		System.out.println("Day: "+tempFocusedDay);
+		
+		tempFocusedMonth = date.substring(5, 7);
+		newMonthDate = Integer.parseInt(tempFocusedMonth);
+		System.out.println("Month: "+tempFocusedMonth);
+		
+		tempFocusedYear = date.substring(0, 4);
+		newYearDate = Integer.parseInt(tempFocusedYear);
+		System.out.println("Year: "+tempFocusedYear);
+		
 
 		String[] dayName = { "Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Lördag", "Söndag" };
 
 		for (int i = 0; i < 7; i++) {
-			
+
 			weekDaysPanel = new JPanel();
 			weekDaysPanel.setPreferredSize(new Dimension(165, 700));
 			weekDaysPanel.setVisible(true);
@@ -58,26 +68,41 @@ public class WeekView extends JPanel {
 			weekDaysPanel.add(weekDaysLabel);
 
 		}
-		
-		cal.set(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), Integer.parseInt(tempFocusedDate));
 
+		//cal.set(newYearDate = Integer.parseInt(tempFocusedYear), newMonthDate = Integer.parseInt(tempFocusedMonth), newDayDate = Integer.parseInt(tempFocusedDay));
+		
+		
+		
+		System.out.println(newYearDate + " " + cal.get(Calendar.YEAR));
+		System.out.println(newMonthDate + " " + cal.get(Calendar.MONTH));
+		System.out.println(newDayDate);
+		
+		System.out.println(cal.get(Calendar.DAY_OF_WEEK));
+		
+		cal.set(newYearDate, (newMonthDate - 1), newDayDate);
+		
 		cal.set(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH),
 				cal.get(Calendar.DAY_OF_MONTH) - (cal.get(Calendar.DAY_OF_WEEK) - 2));
 		
 		
 
 		for (int i = cal.get(Calendar.DAY_OF_MONTH); i < cal.get(Calendar.DAY_OF_MONTH) + 7; i++) {
+			
 
 			dayOfMonthLabel = new JLabel();
 			dayOfMonthLabel.setText("" + i);
-			
+
 			weekDatePanel = new JPanel();
 			weekDatePanel.setPreferredSize(new Dimension(165, 25));
 			weekDatePanel.setVisible(true);
 			
+			
+
 			add(weekDatePanel);
 			weekDatePanel.add(dayOfMonthLabel);
 			
+			
+
 		}
 
 	}
